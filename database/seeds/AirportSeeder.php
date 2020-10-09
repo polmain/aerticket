916fe8647014bbbc5aa016df3039a7e6c22c6536
Polmain <polmain8@gmail.com>
2020-10-09 17:58:22 +0300

    Create Airport Factory

---
+++ database/seeds/AirportSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Airport;
+use Illuminate\Database\Seeder;
+
+class AirportSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Airport::class, 1000)->create();
+    }
+}
